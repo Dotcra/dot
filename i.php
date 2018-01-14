a76b86b2235c80e5fecc6c3e5de6e06283318233
Dot <dotcra@gmail.com>
2018-01-14 13:22:39 +0800

    nth

---
+++ i.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * vim:ft=php
+ * @author Dotcra <dotcra@gmail.com>
+ * @version
+ * @todo
+ */
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+class DOTDocument extends DOMDocument{
+	private $a;
+	private $b;
+	private $c;
+
+
+	public function getElementsByClassName($selector){
+		$xpath = new DOMXpath($this);
+		return $xpath->query($this->cssToXPath("." . $selector));
+	}
+
+	/**
+	 * 
+	 */
+	public function querySelectorAll($selector){
+		$xpath = new DOMXpath($this);
+		return $xpath->query($this->cssToXPath($selector));
+	}
+
+	/**
+	 * @selecter 
+	 */
+	public function querySelector($selector){
+		$xpath = new DOMXpath($this);
+		return $xpath->query($this->cssToXPath($selector))->item(0);
+	}
+
+	public function cssToXPath($selector){
+		$converter=new CssSelectorConverter;
+		return $converter->toXPath($selector);
+
+	}
+}
+
+$a=new DOTDocument;
+$a->loadHTMLFile('tpb.html');
+//var_dump($a->querySelectorAll('.detName')->item(0));
+//var_dump($a->querySelectorAll('.detName'));
+var_dump($a->getElementsByClassName('detName'));

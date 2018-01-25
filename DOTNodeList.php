cea23533b3585ded9d2e6ba1ecf45bdfb218f868
Dot <dotcra@gmail.com>
2018-01-26 00:09:25 +0800

    nth

---
+++ DOTNodeList.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * vim:ft=php
+ * @author Dotcra <dotcra@gmail.com>
+ * @version
+ * @todo
+ */
+
+namespace Dotcra\Dot;
+
+class DOTNodeList extends \DOMNodeList{
+	function __construct(){
+	}
+
+	function __destruct(){
+	}
+}
